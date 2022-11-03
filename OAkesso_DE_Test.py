b2ad5a5241a9eddd3287216ef9f7e621a47a8053
Oakesso <olivierakesso@gmail.com>
2022-11-03 22:37:34 +0100

    update script

--- OAkesso_DE_Test.py
+++ OAkesso_DE_Test.py
@@ -4,3 +4,10 @@ Created on Wed Nov  2 10:31:24 2022
 
 @author: oliakess
 """
+"""
+This code works on 4 data files:
+    - drugs.csv
+    - pubmed.csv
+    - pubmed.json
+    - clinical_trials.csv
+"""
